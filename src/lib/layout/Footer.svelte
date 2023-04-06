db88b59e75f56ba15b70c432fabed2dce84625c4
anbork <anbork@example.com>
2023-04-07 00:45:16 +0300

    Update translations

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { locale } from 'svelte-i18n'
+
+  $: basepath = $page.url.pathname.replace('/' + $locale, '');
 </script>
 
 <ul class="nav justify-content-center">
   <li class="nav-item">
-    <a class="nav-link" class:link-dark={$locale == 'en'} href="/" data-sveltekit-preload-data="tap">English</a>
+    <a class="nav-link" class:link-dark={$locale == 'en'} href={`${basepath || '/'}`} data-sveltekit-preload-data="off">English </a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" class:link-dark={$locale == 'es'} href="/es" data-sveltekit-preload-data="tap">Español</a>
+    <a class="nav-link" class:link-dark={$locale == 'es'} href={`/es${basepath}`} data-sveltekit-preload-data="off">Español</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" class:link-dark={$locale == 'de'} href="/de" data-sveltekit-preload-data="tap">Deutsch</a>
+    <a class="nav-link" class:link-dark={$locale == 'de'} href={`/de${basepath}`} data-sveltekit-preload-data="off">Deutsch</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" class:link-dark={$locale == 'ru'} href="/ru" data-sveltekit-preload-data="tap">Русский</a>
+    <a class="nav-link" class:link-dark={$locale == 'ru'} href={`/ru${basepath}`} data-sveltekit-preload-data="off">Русский</a>
   </li>
 </ul>
--- src/lib/layout/Header.svelte
+++ src/lib/layout/Header.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { locale } from 'svelte-i18n'
+	import { _, locale } from 'svelte-i18n'
   import { langUrl } from '$lib/i18n';
 </script>
 
 <ul class="nav justify-content-center py-3">
   <li class="nav-item">
-    <a class="nav-link" class:link-dark={$page.url.pathname == langUrl('/', $locale)} href={langUrl('/', $locale)}>Home</a>
+    <a class="nav-link" class:link-dark={$page.url.pathname == langUrl('/', $locale)} href={langUrl('/', $locale)}>{$_('navigation.home_link_name')}</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" class:link-dark={$page.url.pathname == langUrl('/generator', $locale)} href={langUrl('/generator', $locale)}>Generator</a>
+    <a class="nav-link" class:link-dark={$page.url.pathname == langUrl('/generator', $locale)} href={langUrl('/generator', $locale)}>{$_('navigation.page_generator_link_name')}</a>
   </li>
 </ul>
