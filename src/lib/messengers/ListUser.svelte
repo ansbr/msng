6b9955c1a2f9065b42afd026084080a4f15bbe68
anbork <anbork@example.com>
2023-04-29 23:08:24 +0300

    Add QR code to wechat

--- src/lib/messengers/ListUser.svelte
+++ src/lib/messengers/ListUser.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Qrcode from "$lib/qrcode/Qrcode.svelte";
   import type { MessengerType } from "$lib/types/MessengerType";
   import Button from "./Button.svelte";  
   export let messengers: MessengerType[] = []
@@ -8,14 +9,20 @@
 <div class="row justify-content-center">
     {#each messengers as messenger (messenger)}
       {#if isSingle}
-        <div class="col px-1 text-center">
-          <Button messenger={messenger} height={128} class="px-3 mt-2 rounded-4" target='_blank'></Button>
-          <h5 class="pt-2">{messenger.title}</h5>
-          
-        </div>
+        {#if messenger.name == 'wechat'}
+          <div class="col text-center">
+            <Qrcode value={messenger.value} width={160} />
+            <h5 class="pt-2">Wechat QR Code</h5>
+          </div>
+        {:else}
+          <div class="col px-1 text-center">
+            <Button messenger={messenger} height={128} class="px-3 mt-2 rounded-4" target='_blank' href={messenger.value}></Button>
+            <h5 class="pt-2">{messenger.title}</h5>
+          </div>
+        {/if}
       {:else}
         <div class="col-4 col-sm-3 col-md-2 col-xl-1 px-1 text-center">
-          <Button messenger={messenger} height={38} class="px-2 mt-2 rounded-3" target='_blank'></Button>
+          <Button messenger={messenger} height={38} class="px-2 mt-2 rounded-3" target='_blank' href={messenger.value}></Button>
           <h6 class="pt-2">{messenger.title}</h6>
         </div>
       {/if}
---
+++ src/lib/qrcode/Qrcode.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import QRCode from "qrcode";
+
+  export let value: string | '' = '';
+  export let width: number = 100;
+
+  const generate = (canvas: HTMLElement) => {
+    QRCode.toCanvas(canvas, value, {
+      width: width*1.2,
+      margin: 0
+    }, (error) => {
+      error && console.error(error)
+    });
+  }
+</script>
+
+<canvas use:generate />
